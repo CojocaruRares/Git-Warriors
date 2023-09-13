2f41230cca6c17dad13f960e6be1d6188d18a8f3
CojocaruRares <rares.cojocaru147@gmail.com>
2023-09-13 15:53:13 +0300

    forgot to commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import SearchUser from './components/Search/SearchUser';
+import Battle from './components/Battle/Battle';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/SearchUser' element={<SearchUser/>} />
+          <Route path='/Battle' element={<Battle/>} />
         </Routes>
       </div>
     </Router>
